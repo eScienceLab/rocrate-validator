c34fde8955037fd8ceb3e67fcc65d695adcab96a
Ettore Murabito <ettore.murabito@manchester.ac.uk>
2025-10-28 09:50:34 +0000

    Removed double imports

--- tests/integration/profiles/five-safes-crate/test_5src_funding.py
+++ tests/integration/profiles/five-safes-crate/test_5src_funding.py
@@ -17,8 +17,6 @@ import logging
 from rocrate_validator.models import Severity
 from tests.ro_crates import ValidROC
 from tests.shared import do_entity_test, SPARQL_PREFIXES
-from tests.ro_crates import ValidROC
-from tests.shared import do_entity_test, SPARQL_PREFIXES
 
 # set up logging
 logger = logging.getLogger(__name__)
