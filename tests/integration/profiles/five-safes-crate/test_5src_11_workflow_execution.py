eaa0f9d7391f6b3146f77df91f2250f334675e20
EttoreM <ettore.murabito@manchester.ac.uk>
2026-03-27 11:59:15 +0000

    PARTIALLY amended the error messages that appear as validation fails. TO BE COMPLETED.

--- tests/integration/profiles/five-safes-crate/test_5src_11_workflow_execution.py
+++ tests/integration/profiles/five-safes-crate/test_5src_11_workflow_execution.py
@@ -47,7 +47,7 @@ def test_5src_workflow_object_with_no_name():
         expected_validation_result=False,
         expected_triggered_requirements=["WorkflowExecution"],
         expected_triggered_issues=[
-            "WorkflowRun MUST have a name string of at least 10 characters."
+            "The `CreateAction` corresponding to the workflow run MUST have a name string of at least 10 characters."
         ],
         profile_identifier="five-safes-crate",
         rocrate_entity_mod_sparql=sparql,
@@ -78,7 +78,7 @@ def test_5src_workflow_object_with_name_not_string():
         expected_validation_result=False,
         expected_triggered_requirements=["WorkflowExecution"],
         expected_triggered_issues=[
-            "WorkflowRun MUST have a name string of at least 10 characters."
+            "The `CreateAction` corresponding to the workflow run MUST have a name string of at least 10 characters."
         ],
         profile_identifier="five-safes-crate",
         rocrate_entity_mod_sparql=sparql,
@@ -109,7 +109,7 @@ def test_5src_workflow_object_with_not_long_enough_name():
         expected_validation_result=False,
         expected_triggered_requirements=["WorkflowExecution"],
         expected_triggered_issues=[
-            "WorkflowRun MUST have a name string of at least 10 characters."
+            "The `CreateAction` corresponding to the workflow run MUST have a name string of at least 10 characters."
         ],
         profile_identifier="five-safes-crate",
         rocrate_entity_mod_sparql=sparql,
@@ -203,7 +203,7 @@ def test_5src_workflow_object_with_no_action_status():
         expected_triggered_requirements=["WorkflowExecution"],
         expected_triggered_issues=[
             (
-                "WorkflowRun MUST have an actionStatus "
+                "`CreateAction` MUST have an actionStatus "
                 "with an allowed value (see https://schema.org/ActionStatusType)."
             )
         ],
@@ -236,7 +236,7 @@ def test_5src_workflow_object_with_no_properly_valued_action_status():
         expected_triggered_requirements=["WorkflowExecution"],
         expected_triggered_issues=[
             (
-                "WorkflowRun MUST have an actionStatus "
+                "`CreateAction` MUST have an actionStatus "
                 "with an allowed value (see https://schema.org/ActionStatusType)."
             )
         ],
