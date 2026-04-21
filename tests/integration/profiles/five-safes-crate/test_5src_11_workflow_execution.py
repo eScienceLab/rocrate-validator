f770e4f81478a4aa9f5d167dfd8a3661c52646e5
EttoreM <ettore.murabito@manchester.ac.uk>
2026-04-21 10:53:00 +0100

    Improved comment in 11_workflow_execution_phase.ttl

--- tests/integration/profiles/five-safes-crate/test_5src_11_workflow_execution.py
+++ tests/integration/profiles/five-safes-crate/test_5src_11_workflow_execution.py
@@ -267,7 +267,7 @@ def test_5src_workflow_object_not_mentioned_by_root_data_entity():
         expected_validation_result=False,
         expected_triggered_requirements=["RootDataEntity"],
         expected_triggered_issues=[
-            "RootDataEntity SHOULD reference a workflow run entity (typed CreateAction)."
+            "RootDataEntity SHOULD reference a workflow run entity (typed CreateAction) through `mentions`."
         ],
         profile_identifier="five-safes-crate",
         rocrate_entity_mod_sparql=sparql,
